8db4075eb043221cf3496c93873162f74ee4478f
LukaDjak <79627017+LukaDjak@users.noreply.github.com>
2024-03-31 10:26:21 +0200

    prettier code

--- SpaDomacaZadaca01/Cvijet.cpp
+++ SpaDomacaZadaca01/Cvijet.cpp
@@ -2,32 +2,36 @@
 Cvijet::Cvijet(sf::RenderWindow* window)
 {
 	this->window = window;
-	t = frameClock.restart();
+	elapsed = frameClock.restart();
 }
 
 void Cvijet::draw()
 {
+	crtajNebo();
+	crtajList();
+	crtajStabljiku();
+	crtajLatice();
+	crtajShape();
+	crtajGround();
+	crtajSunce();
+
+	//time management
+	elapsed = frameClock.restart();
+	scale += scaleSpeed * elapsed.asSeconds();
+	if (scale >= 2.0f || scale <= 1.0f)
+		scaleSpeed = -scaleSpeed;
+}
+
+void Cvijet::crtajNebo()
+{
 	//blue sky background
-	sf::RectangleShape sky(sf::Vector2f(800, 600));
+	sky.setSize(sf::Vector2f(800, 600));
 	sky.setFillColor(sf::Color(153, 228, 255));
 	window->draw(sky);
-
-	//the upper green part of the cvijet
-	sf::RectangleShape upperLine(sf::Vector2f(10.f, 200.f));
-	upperLine.setFillColor(sf::Color(44, 150, 59));
-	upperLine.setPosition(100.f, 150.f);
-	upperLine.rotate(-10);
-	window->draw(upperLine);
-
-	//the lower  green part of the cvijet
-	sf::RectangleShape lowerLine(sf::Vector2f(10.f, 150.f));
-	lowerLine.setFillColor(sf::Color(44, 150, 59));
-	lowerLine.setPosition(135.f, 343.f);
-	lowerLine.rotate(10);
-	window->draw(lowerLine);
-
+}
+void Cvijet::crtajList()
+{
 	//the leaf
-	sf::ConvexShape leaf;
 	leaf.setPointCount(5);
 	leaf.setPoint(0, sf::Vector2f(140, 340));
 	leaf.setPoint(1, sf::Vector2f(200, 310));
@@ -36,25 +40,47 @@ void Cvijet::draw()
 	leaf.setPoint(4, sf::Vector2f(190, 360));
 	leaf.setFillColor(sf::Color(45, 128, 13));
 	window->draw(leaf);
+}
+void Cvijet::crtajStabljiku()
+{
+	//the upper green part of the cvijet
+	upperLine.setSize(sf::Vector2f(10.f, 200.f));
+	upperLine.setFillColor(sf::Color(44, 150, 59));
+	upperLine.setPosition(100.f, 150.f);
+	upperLine.setRotation(-10);
+	window->draw(upperLine);
 
+	//the lower green part of the cvijet
+	lowerLine.setSize(sf::Vector2f(10.f, 150.f));
+	lowerLine.setFillColor(sf::Color(44, 150, 59));
+	lowerLine.setPosition(135.f, 343.f);
+	lowerLine.setRotation(10);
+	window->draw(lowerLine);
+}
+void Cvijet::crtajLatice()
+{
 	//red circles
 	for (int i = 0; i < 5; i++)
 	{
-		sf::CircleShape shape(30.f);
-		shape.setFillColor(sf::Color(255, 0, 0));
-		shape.setPosition(100, 100);
-		shape.rotate((i + 1) * 72.f);
-		window->draw(shape);
+		sf::CircleShape latice;
+		latice.setRadius(30.f);
+		latice.setFillColor(sf::Color(255, 0, 0));
+		latice.setPosition(100, 100);
+		latice.setRotation((i + 1) * 72.f);
+		window->draw(latice);
 	}
-
+}
+void Cvijet::crtajShape()
+{
 	//yellow circle
-	sf::CircleShape shape(35.f);
+	shape.setRadius(35.f);
 	shape.setFillColor(sf::Color(255, 255, 0));
 	shape.setPosition(65.f, 65.f);
 	window->draw(shape);
-
+}
+void Cvijet::crtajGround()
+{
 	//ground
-	sf::ConvexShape ground;
 	ground.setPointCount(4);
 	ground.setPoint(0, sf::Vector2f(0, 450));
 	ground.setPoint(1, sf::Vector2f(0, 600));
@@ -62,25 +88,20 @@ void Cvijet::draw()
 	ground.setPoint(3, sf::Vector2f(800, 475));
 	ground.setFillColor(sf::Color(45, 128, 13));
 	window->draw(ground);
-
+}
+void Cvijet::crtajSunce()
+{
 	//sunce
-	sf::CircleShape sunce(100.f);
+	sunce.setRadius(100.f);
 	sunce.setFillColor(sf::Color(255, 255, 0));
 	sunce.setPosition(750, -50);
 	window->draw(sunce);
 
-	//animation time management
-	sf::Time elapsed = frameClock.restart();
-	scale += scaleSpeed * elapsed.asSeconds();
-	if (scale >= 2.0f || scale <= 1.0f)
-		scaleSpeed = -scaleSpeed;
-
 	//kinda animated zrake sunca
-	for (size_t i = 0; i < 7; i++)
+	for (auto i = 0; i < 7; i++)
 	{
 		sf::Vector2f size(10.f, (i % 2 == 0) ? 10.f : 7.f);
-		sf::RectangleShape zraka(size);
-
+		zraka.setSize(size);
 		zraka.setScale(1.f, 30 * scale);
 		zraka.setOrigin(zraka.getSize() / 2.0f);
 		zraka.setFillColor(sf::Color(255, 255, 0));
--- SpaDomacaZadaca01/Cvijet.h
+++ SpaDomacaZadaca01/Cvijet.h
@@ -1,16 +1,31 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-#include <iostream>
 class Cvijet
 {
 private:
 	sf::RenderWindow* window;
 	sf::Clock frameClock;
-	sf::Time t;
+	sf::Time elapsed;
 	float scale = 1.f;
 	float scaleSpeed = .5f;
+
+	sf::RectangleShape sky;
+	sf::RectangleShape upperLine;
+	sf::RectangleShape lowerLine;
+	sf::ConvexShape leaf;
+	sf::CircleShape shape;
+	sf::ConvexShape ground;
+	sf::CircleShape sunce;
+	sf::RectangleShape zraka;
 public:
 	Cvijet(sf::RenderWindow* window);
 	void draw();
+	void crtajNebo();
+	void crtajStabljiku();
+	void crtajList();
+	void crtajLatice();
+	void crtajShape();
+	void crtajGround();
+	void crtajSunce();
 };
 
